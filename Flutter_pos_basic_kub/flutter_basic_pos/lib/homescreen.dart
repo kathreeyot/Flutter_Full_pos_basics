94d9be5f96e8393706ea0a9fb56ad687886df170
kathreeyot <kathreeyot@gmail.com>
2023-05-24 17:03:21 +0700

    comiits

---
+++ Flutter_pos_basic_kub/flutter_basic_pos/lib/homescreen.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'menu_item.dart';
+import 'database.dart';
+import 'image_picker.dart';
+void main() => runApp( HomeScreen());
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final Database _database = Database();
+  final ImagePickerService _imagePickerService = ImagePickerService();
+  List<MenuItem> menuItems = [];
+  List<MenuItem> selectedItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMenuItems();
+  }
+
+  Future<void> _loadMenuItems() async {
+    menuItems = await _database.loadMenuItems();
+    setState(() {});
+  }
+
+  Future<void> _saveMenuItems() async {
+    await _database.saveMenuItems(menuItems);
+  }
+
+  Future<void> _pickImage(int index) async {
+    final pickedImage = await _imagePickerService.pickImageFromGallery();
+    setState(() {
+      if (pickedImage != null) {
+        menuItems[index].image = pickedImage;
+      }
+    });
+  }
+
+  void _editMenuItem(MenuItem menuItem) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String editedName = menuItem.name;
+        String editedDescription = menuItem.description;
+        double editedPrice = menuItem.price;
+
+        return AlertDialog(
+          title: const Text('Edit Menu Item'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                onChanged: (value) {
+                  editedName = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                onChanged: (value) {
+                  editedDescription = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Price'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  editedPrice = double.tryParse(value) ?? 0.0;
+                },
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text('Pick Image'),
+                    onPressed: () {
+                      _pickImage(menuItems.indexOf(menuItem));
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Save'),
+              onPressed: () {
+                setState(() {
+                  menuItem.name = editedName;
+                  menuItem.description = editedDescription;
+                  menuItem.price = editedPrice;
+                });
+                Navigator.of(context).pop();
+                _saveMenuItems();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteMenuItem(MenuItem menuItem) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Menu Item'),
+          content: const Text('Are you sure you want to delete this item?'),
+          actions: [
+            ElevatedButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                setState(() {
+                  menuItems.remove(menuItem);
+                });
+                Navigator.of(context).pop();
+                _saveMenuItems();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addMenuItem() {
+    setState(() {
+      menuItems.add(MenuItem('New Item', 'Description', 0.0, null));
+    });
+    _saveMenuItems();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Restaurant POS'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: menuItems.length,
+              itemBuilder: (ctx, index) {
+                final menuItem = menuItems[index];
+                return ListTile(
+                  leading: menuItem.image != null
+                      ? CircleAvatar(
+                          backgroundImage: FileImage(menuItem.image!),
+                        )
+                      : const Icon(Icons.image),
+                  title: Text(menuItem.name),
+                  subtitle: Text(menuItem.description),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(menuItem.price.toString()),
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          _editMenuItem(menuItem);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteMenuItem(menuItem);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          const Divider(),
+          const Text('Selected Items'),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: selectedItems.length,
+            itemBuilder: (ctx, index) {
+              return ListTile(
+                title: Text(selectedItems[index].name),
+                subtitle: Text(selectedItems[index].description),
+                trailing: Text('\$${selectedItems[index].price.toStringAsFixed(2)}'),
+              );
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Place Order'),
+            onPressed: () {
+              // Handle order placement logic
+            },
+          ),
+          ElevatedButton(
+            onPressed: _addMenuItem,
+            child: const Text('Add Item'),
+          ),
+        ],
+      ),
+    );
+  }
+}
